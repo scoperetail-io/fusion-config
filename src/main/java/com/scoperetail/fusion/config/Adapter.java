0a510ab704df89d37ad800ac9eb8966f9464728a
ravi-zadpide <ravi.zadpide@scoperetail.com>
2021-08-11 15:22:16 +0530

    sonar code smell fix.

--- src/main/java/com/scoperetail/fusion/config/Adapter.java
+++ src/main/java/com/scoperetail/fusion/config/Adapter.java
@@ -13,10 +13,10 @@ package com.scoperetail.fusion.config;
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -70,7 +70,7 @@ public class Adapter {
   }
 
   //COMMON
-  public TransformationType transformationType;
+  private TransformationType transformationType;
   private AdapterType adapterType;
   private TransportType trasnportType;
   private UsecaseResult usecaseResult;
